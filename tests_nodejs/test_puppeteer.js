cd51a42d7a6502df1ae17873421177c3a50d9d7e
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-09-23 01:22:28 +0200

    - add test for nodeJS puppeteer stealth - update puppeteer stealth stack signature

--- tests_nodejs/test_puppeteer.js
+++ tests_nodejs/test_puppeteer.js
@@ -1,5 +1,9 @@
 import puppeteer from 'puppeteer';
-import {__server_url__, sleep} from './utils.js'
+import puppeteerExtra from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import {__server_url__, sleep} from './utils.js';
+
+puppeteerExtra.use(StealthPlugin())
 
 const script = async ()=>{
     await brotector.init_done
@@ -19,11 +23,29 @@ async function test(browser){
 async function test_puppeteer(){
     const browser = await puppeteer.launch({ headless: false });
     try{
-        test(browser)
+        await test(browser)
     }finally{
         await browser.close();
     }
 
 }
+async function test_puppeteerExtraStealth(){
+    const browser = await puppeteerExtra.launch({ headless: false });
+    try{
+        await test(browser)
+    }catch(e){
+        if(e.name === "RangeError" && e.message === 'Maximum call stack size exceeded' 
+            && e.stack.match("\n    at SeleniumScriptInjectionHandler")){
+                // infinite recursion induced by puppeteer stealth https://github.com/berstend/puppeteer-extra/blob/39248f1f5deeb21b1e7eb6ae07b8ef73f1231ab9/packages/puppeteer-extra-plugin-stealth/evasions/_utils/index.js#L42
+            }else{
+                throw e
+            };
+    }finally{
+        await browser.close();
+    };
+
+}
 
 await test_puppeteer()
+await test_puppeteerExtraStealth()
+process.exit(0)
